3ff00e08b3c359eab0379746070284226c08dfbd
Rubén Fernández Galán <ruben.fernandez@ciberexperis.es>
2019-03-25 17:01:58 +0100

    Aplicacion miscelaneos

---
+++ 06-miscelaneos/src/app/components/usuario/usuario.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent implements OnInit {
+
+  constructor( private router: ActivatedRoute) {
+    this.router.params.subscribe( params => {
+      console.log('RUTA PADRE');
+      console.log(params);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
